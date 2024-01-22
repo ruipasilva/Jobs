1d0b7b2356dc1b296fc487df9b72c1201ebf264b
Rui Silva <rui.silva@xdesign.com>
2024-01-22 16:45:03 +0000

    Initial commit

---
+++ Jobs/Jobs/JobsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobsApp.swift
+//  Jobs
+//
+//  Created by Rui Silva on 22/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct JobsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
