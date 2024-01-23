5ed53462ad633220bcc49be57fcd990251c0c215
Rui Silva <rui.silva@xdesign.com>
2024-01-23 16:41:25 +0000

    Change SwiftData Container

--- Jobs/Jobs/JobsApp.swift
+++ Jobs/Jobs/JobsApp.swift
@@ -10,14 +10,22 @@ import SwiftData
 
 @main
 struct JobsApp: App {
+    let container: ModelContainer
     var body: some Scene {
         WindowGroup {
             MainView()
         }
-        .modelContainer(for: Job.self)
+        .modelContainer(container)
     }
     
     init() {
+        let schema = Schema([Job.self])
+        let config = ModelConfiguration("MyJobs", schema: schema)
+        do {
+            container = try ModelContainer(for: schema, configurations: config)
+        } catch {
+            fatalError("Could not configure container - try uninstalling the app if issues occur")
+        }
         print(URL.applicationSupportDirectory.path(percentEncoded: false))
     }
 }
