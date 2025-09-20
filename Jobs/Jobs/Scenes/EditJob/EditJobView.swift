b3538959262b58da44e0ceabab346e7adea442e8
Rui Silva <ruipasilva@gmail.com>
2025-09-20 23:14:00 +0100

    Test complete

--- Jobs/Jobs/Scenes/EditJob/EditJobView.swift
+++ Jobs/Jobs/Scenes/EditJob/EditJobView.swift
@@ -66,7 +66,6 @@ struct EditJobView: View {
             editJobViewModel.initialCompanyName = editJobViewModel.job.company
             editJobViewModel.initialJobTitle = editJobViewModel.job.title
         })
-        //test
         .task(id: job) {
             do {
                 try context.save()
